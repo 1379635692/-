2d7754e498344d5ad3534a8c45505ca9e98e1a54
xiangwei <1379635692@qq.com>
2019-07-15 17:30:33 +0800

    第一次远程仓库连接

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="我是爸爸传给儿子的文字"/>
   </div>
 </template>
 

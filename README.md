6c33796f60a2881307218e7a89e0c4a71c2aac2b
xiangwei <1379635692@qq.com>
2019-07-15 17:56:55 +0800

    基本简介提交

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# demo
+# 全栈美团网
 
+>*  前端vue ， 后端koa2， 目前自己一个人着手的全栈项目，希望我的奉献对大家有帮助，我会持续更新的。
 ## Project setup
 ```
 npm install
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,6 +14,14 @@ export default {
 </script>
 
 <style scoped>
+    .hello{
+        width: 400px;
+        height: 400px;
+        margin:  0 auto;
+        text-align: center;
+        border: 1px solid red;
+        font-size: 20px;
+    }
 p{
     color: red;
 }
